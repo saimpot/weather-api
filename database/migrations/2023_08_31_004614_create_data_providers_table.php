481c25c3c2efa8105ee5231944ed75470cde54b8
saimpot <saimpot@gmail.com>
2023-08-31 04:13:53 +0300

    🚀 :sparkles: Add migrations for services and data providers tables
    🎯 :file_folder: database/migrations/2023_08_31_004700_create_services_table.php
    
    🎯 :file_folder: database/migrations/2023_08_31_004614_create_data_providers_table.php

--- database/migrations/2023_08_31_004614_create_data_providers_table.php
+++ database/migrations/2023_08_31_004614_create_data_providers_table.php
@@ -4,16 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('data_providers', function (Blueprint $table) {
+        Schema::create('data_providers', static function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
+            $table->foreignIdFor('services', 'service_id');
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
--- database/migrations/2023_08_31_004700_create_services_table.php
+++ database/migrations/2023_08_31_004700_create_services_table.php
@@ -4,16 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('services', function (Blueprint $table) {
+        Schema::create('services', static function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
