001d6086927d4bd7d8d6062fe07c0101689a413d
saimpot <saimpot@gmail.com>
2023-08-31 04:09:05 +0300

    ✨️📦 Create weather data table migration
    - Add foreign key constraints for location and weather data type
    - Add `value` column
    - Add `created_at` and `updated_at` timestamps

--- database/migrations/2023_08_31_001637_create_weather_data_table.php
+++ database/migrations/2023_08_31_001637_create_weather_data_table.php
@@ -4,16 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('weather_data', function (Blueprint $table) {
+        Schema::create('weather_data', static function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignIdFor('location', 'location_id');
+            $table->foreignIdFor('weather_data_type', 'type_id');
+            $table->string('value');
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
