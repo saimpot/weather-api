5ae27f2be3751b967595269ba4727a49fdbfcba7
saimpot <saimpot@gmail.com>
2023-08-31 04:10:49 +0300

    🚀 :tada: Create weather data type table migration
    The migration creates a new table called `weather_data_type` with the following columns:
    - `id` (primary key)
    - `type` (string)
    - `created_at` (timestamp, set to current time)
    - `updated_at` (timestamp, set to current time and updated on update)
    
    This migration is responsible for creating the table to store weather data types.

--- database/migrations/2023_08_31_004303_create_weather_type_table.php
+++ database/migrations/2023_08_31_004303_create_weather_type_table.php
@@ -4,16 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('weather_type', function (Blueprint $table) {
+        Schema::create('weather_data_type', static function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('type');
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
